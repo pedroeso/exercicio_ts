f9d93b2bca869ecfa70c36604fd0cc6874e83046
Pedro Ribeiro Da Rocha <131209343+pedroeso@users.noreply.github.com>
2024-07-12 17:53:42 -0300

    esta pronto!

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(name) {
+    return "Olá " + name;
+}
+const resultado = multiplicar(3, 4);
+const saudacao = saudar("Pedro");
+console.log(resultado);
+console.log(saudacao);
---
+++ src/exercicio.ts
@@ -0,0 +1,16 @@
+
+function multiplicar(a: number, b: number): number {
+    return a * b;
+    }
+    
+    
+    function saudar(name: string): string {
+    return "Olá " + name;
+    }
+    
+        
+    const resultado = multiplicar(3, 4); 
+    const saudacao = saudar("Pedro"); 
+    
+    console.log(resultado);
+    console.log(saudacao);
